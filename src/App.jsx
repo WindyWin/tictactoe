af2b216d7dbe6a28233206a46e1558a462aa76ee
WindyWin <19521362@gm.uit.edu.vn>
2023-10-16 23:04:34 +0700

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import { useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import './App.css'
 import { ItemBlock } from './component/ItemBlock'
 
 const winningCondition = [
-  [[0, 0], [0, 1], [0, 2]],
-  [[1, 0], [1, 1], [1, 2]],
-  [[2, 0], [2, 1], [2, 2]],
-  [[0, 0], [1, 0], [2, 0]],
-  [[0, 1], [1, 1], [2, 1]],
-  [[0, 2], [1, 2], [2, 2]],
-  [[0, 0], [1, 1], [2, 2]],
-  [[0, 2], [1, 1], [2, 0]]
+  [[0, 0], [0, 1], [0, 2]], // row 1
+  [[1, 0], [1, 1], [1, 2]], // row 2
+  [[2, 0], [2, 1], [2, 2]], // row 3
+  [[0, 0], [1, 0], [2, 0]], // col 1
+  [[0, 1], [1, 1], [2, 1]], // col 2
+  [[0, 2], [1, 2], [2, 2]], // col 3
+  [[0, 0], [1, 1], [2, 2]], // cross 1
+  [[0, 2], [1, 1], [2, 0]], // cross 2
 ]
 
 function App() {
@@ -21,22 +21,50 @@ function App() {
   ])
   const [currentPlayer, setCurrentPlayer] = useState('X')
   const [step, setStep] = useState(0)
+  const [message, setMessage] = useState('')
 
+  useEffect(() => {
+    for (let i = 0; i < winningCondition.length; i++) {
+      let count = 0;
+      for (let j = 0; j < winningCondition[i].length; j++) {
+        let [row, col] = winningCondition[i][j];
+        if (ticTacToeMap[row][col] === currentPlayer) {
+          count++;
+        }
+      }
+      if (count === 3) {
+        // alert(`${currentPlayer} is winner`);
+        setMessage(`${currentPlayer} is winner`)
+        setTimeout(() => {
+          resetGame()
+        }, 1000)
+
+      }
+    }
+
+
+  }, [currentPlayer, ticTacToeMap])
+  useEffect(() => {
+
+    if (step === 9 && message === '') {
+      setMessage('Draw')
+      setTimeout(() => {
+        resetGame()
+      }, 1000)
+
+    }
+
+
+  }, [step])
 
   const onItemClick =
     (rowIndex, colIndex) => () => {
-      if(ticTacToeMap[rowIndex][colIndex] !== '') return;
+      if (ticTacToeMap[rowIndex][colIndex] !== '') return;
       let newMap = [...ticTacToeMap]
       newMap[rowIndex][colIndex] = currentPlayer
       setTicTacToeMap(newMap)
       setCurrentPlayer(currentPlayer === 'O' ? 'X' : 'O')
       setStep(step + 1);
-      if (checkWinner()) {
-        resetGame()
-      }
-      else {
-        checkDraw();
-      }
     }
 
 
@@ -48,34 +76,13 @@ function App() {
     ])
     setCurrentPlayer('X')
     setStep(0)
+    setMessage('')
   }
 
-  const checkWinner = () => {
-    for (let i = 0; i < winningCondition.length; i++) {
-      let count = 0;
-      for (let j = 0; j < winningCondition[i].length; j++) {
-        let [row, col] = winningCondition[i][j];
-        if (ticTacToeMap[row][col] === currentPlayer) {
-          count++;
-        }
-      }
-      if (count === 3) {
-        alert(`${currentPlayer} is winner`);
-        return true;
-      }
-    }
-    return false;
-
-  }
-  const checkDraw = () => {
-    if (step === 8) {
-      alert('Draw');
-      resetGame()
-    }
-  }
 
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center',height:'100vh' }}>
+    <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <p style={{ marginBottom: "1 rem" }}>{message}</p>
       {ticTacToeMap.map((row, rowIndex) => {
         return <div key={rowIndex} style={{ display: 'flex' }}>{
           row.map((item, colIndex) => (
@@ -87,6 +94,7 @@ function App() {
         }</div>
 
       })}
+      <button onClick={resetGame}>Reset</button>
 
     </div>
   )
